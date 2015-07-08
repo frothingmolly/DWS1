8fe20a3d5a7c34ce5b31dd90aa0be5c4e7063ca5
John Pylant <johnathan.pylant@gmail.com>
2015-07-08 10:53:49 -0400

    Created README

---
+++ README.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by JP on 7/8/15.
+ */
+echo "# DWS1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/frothingmolly/DWS1.git
+    git push -u origin master
