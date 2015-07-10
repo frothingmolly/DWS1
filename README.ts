c981c6e634c04c3bee67d5d79c3fb855d20d0a3b
John Pylant <johnathan.pylant@gmail.com>
2015-07-09 22:41:47 -0400

    reorg

--- README.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by JP on 7/8/15.
- */
-echo "# DWS1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/frothingmolly/DWS1.git
-    git push -u origin master
